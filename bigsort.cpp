e317e7a7ea3cb030825d7121a6517df8abe80b47
SanderGH <a.ivanov-sander@yandex.com>
2019-05-14 23:49:03 +0300

    first commit

--- bigsort.cpp
+++ bigsort.cpp
@@ -1,21 +1,40 @@
 // bigsort.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include "pch.h"
 #include <iostream>
+#include <vector>
+#include <list>
+#include <deque>
+#include <chrono>
+
+using namespace std;
+
+template<class T>
+void BubbleSort(vector<T>& vec)
+{
+	int size = vec.size();
+	for( int i = 0; i < size -1 ; i++)
+	{
+		for (int j = 0; j < size - i - 1; j++)
+		{
+			if (vec[j] > vec[j+1])
+				std::swap( vec[j], vec[j+1]);
+		}
+	}
+}
+void map()
+{
+}
+
+void reduce()
+{
+}
 
 int main()
 {
+	std::chrono::duration<int> sss(20);
+	std::ratio<22, 1> k;
+	vector<int> vec{23,5,7,12,89,3,21,12,34,6};
+	BubbleSort(vec);
     std::cout << "Hello World!\n"; 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
